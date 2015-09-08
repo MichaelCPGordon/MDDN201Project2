5ec913319bea6198644729a4fa46de598c70d5bf
Michael <michaelcpgordon@gmail.com>
2015-09-08 18:27:38 +1200

    Added buttons for creating playlists and adding music to queue

--- app/js/controllers/musicCtrl.js
+++ app/js/controllers/musicCtrl.js
@@ -225,6 +225,28 @@ angular.module('controllers.music', [])
         $scope.shuffleIcon = "assets/icons/shuffle.png";
         $scope.repeatIcon = "assets/icons/repeat.png";
 
+        $scope.fabButton = {
+            isOpen: false,
+            direction: "up"
+        };
+
+
+        $scope.topDirections = ['left', 'up'];
+        $scope.bottomDirections = ['down', 'right'];
+        $scope.isOpen = false;
+        $scope.availableModes = ['md-fling', 'md-scale'];
+        $scope.selectedMode = 'md-fling';
+        $scope.availableDirections = ['up', 'down', 'left', 'right'];
+        $scope.selectedDirection = 'up';
+
+
+
+
+
+
+
+
+
 
         $scope.generateRandomGenreBoxColor();
         $scope.orderSongsBy("artist", $scope.music.allSongs);
